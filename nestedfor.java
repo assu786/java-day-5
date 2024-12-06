97915cb0032d9c9e8407ed8610568f747d265b68
assu786 <shaikaqsa.78@gmail.com>
2024-12-06 13:33:07 +0530

    Add files via upload

---
+++ nestedfor.java
@@ -0,0 +1,15 @@
+class Lo
+{
+    public static void main(String args[])
+    {
+        for(int i=0;i<5;i++)
+        {
+            for(int j=0;j<5;j++)
+            {
+                int k=i+j;
+                System.out.print(k);
+            }
+            System.out.println( );
+        }
+    }
+}
